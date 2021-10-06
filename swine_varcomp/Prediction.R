ec06ba4d3054ae1cd6eddaf1f8897bead5ce0b3c
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-10-06 19:59:57 -0400

    Update Prediction.R

--- swine_varcomp/Prediction.R
+++ swine_varcomp/Prediction.R
@@ -50,7 +50,7 @@ for (i in 1:10) {
   # adjust by F
   NBA_Adj<- read.table("/home/varcomp/Landrace_Breed/New_Pipeline/New_Phenotypes/Cross_Validation/NBA_a/yd.Landrace_NBA_add.CV.Adj.variance.components.model.csv", header = T, sep=",")
   NBA_Adj<- as.data.frame(NBA_Adj)
-  NBA_Adj<- NBA_Adj[,c("EGO","F_FIT","BF_S")]
+  NBA_Adj<- NBA_Adj[,c("EGO","F_FIT","NBA_S")]
   names(NBA_Adj)[1]<- "ID"
   y_temp<- merge(y_temp, NBA_Adj, by="ID")
   y_temp$NBA_Adj<- y_temp$NBA - y_temp$F_FIT - y_temp$NBA_S
