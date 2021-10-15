388abe277a1bdda508502120413994b3a7326712
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-10-15 11:15:11 -0400

    Create Landrace.F_TeatCount.Add.Prediction.R

---
+++ swine_varcomp/Landrace.F_TeatCount.Add.Prediction.R
@@ -0,0 +1,67 @@
+library(dplyr)
+library(data.table)
+
+add<- fread("Landrace.add.grm.txt")
+
+Acc<- data.frame(matrix(NA, nrow = 10, ncol = 1))
+
+for (i in 1:10) {
+  F_TeatCount_temp<- read.table(paste("/home/varcomp/Landrace_Breed/New_Pipeline/New_Phenotypes/Cross_Validation/yd.Landrace.F_TeatCount_add.CV", i, ".variance.components.model.csv", sep=""), header = T, sep=",")
+  F_TeatCount_temp_N<- read.table(paste("/home/varcomp/Landrace_Breed/New_Pipeline/New_Phenotypes/Cross_Validation/Landrace.F_TeatCount_N", i, ".csv", sep=""), header = F)
+  
+  F_TeatCount_temp<- F_TeatCount_temp[order(F_TeatCount_temp$EGO),]
+  Losses_temp_N<- Losses_temp_N[order(Losses_temp_N$V1),]
+  
+  # Additive effect
+  add_temp_T<- add[add$V1 %in% F_TeatCount_temp$EGO]
+  add_temp_T<- add_temp_T[order(add_temp_T$V1),]
+  add_temp_T2<- select_if(add_temp_T, colnames(add_temp_T) %in% F_TeatCount_temp$EGO)
+  add_temp_T2<- add_temp_T2 %>% select(sort(names(add_temp_T2)))
+  A_txt<- as.matrix(add_temp_T2)
+  
+  add_temp_N<- add[add$V1 %in% F_TeatCount_temp_N]
+  add_temp_N<- add_temp_N[order(add_temp_N$V1),]
+  add_temp_N2<- select_if(add_temp_N, colnames(add_temp_N) %in% F_TeatCount_temp$EGO)
+  add_temp_N2<- add_temp_N2 %>% select(sort(names(add_temp_N2)))
+  A_nxt<- as.matrix(add_temp_N2)
+  
+  
+  a_t<- as.vector(F_TeatCount_temp$yd_A)
+  a<- A_nxt %*% solve(A_txt) %*% a_t
+  
+ write.table(a, file=paste("/home/varcomp/Landrace_Breed/New_Pipeline/New_Phenotypes/Cross_Validation/F_TeatCount_A/F_TeatCount","_a.csv", sep=""), quote = F, col.names = F, row.names = F)
+  
+  p<- a 
+  p<- cbind(add_temp_N$V1, p)
+  p<- as.data.frame(p)
+  names(p)<- c("ID","PEBV")
+  
+ write.table(p, file=paste("/home/varcomp/Landrace_Breed/New_Pipeline/New_Phenotypes/Cross_Validation/F_TeatCount_A/F_TeatCount",i,"_p.csv", sep=""), quote = F, col.names = F, row.names = F)
+  
+  # get phenotype
+  y<- read.table("Landrace_BirthTotalTeatCount_females_Filtered", header = T, sep = ",")
+  names(y)<- c("ID", "yd")
+ F_TeatCount_temp_N<- as.data.frame(F_TeatCount_temp_N)
+  names(F_TeatCount_temp_N)<- "ID"
+  y_temp<- merge(F_TeatCount_temp_N, y, by="ID")
+  
+  # adjust by F
+  F_TeatCount_Adj<- read.table("/home/varcomp/Landrace_Breed/New_Pipeline/New_Phenotypes/Cross_Validation/yd.Landrace.F_TeatCount_add.CV.Adj.variance.components.model.csv", header = T, sep=",")
+  F_TeatCount_Adj<- as.data.frame(F_TeatCount_Adj)
+  F_TeatCount_Adj<- F_TeatCount_Adj[,c("EGO","F_FIT","yd_S")]
+  names(F_TeatCount_Adj)[1]<- "ID"
+  y_temp<- merge(y_temp,F_TeatCount_Adj, by="ID")
+  y_temp$F_TeatCount_Adj<- y_temp$yd - y_temp$F_FIT - y_temp$yd_S
+  
+  y_temp<- merge(y_temp, p, by="ID")
+  write.table(y_temp, file=paste("/home/varcomp/Landrace_Breed/New_Pipeline/New_Phenotypes/Cross_Validation/F_TeatCount_A/F_TeatCount",i,"_y.csv", sep=""), quote = F, col.names = F, row.names = F, sep=",")
+  
+  
+  corr<- cor(y_temp$PEBV, y_temp$F_TeatCount_Adj, use="p")
+  print(corr)
+  
+  Acc[i,1]<- corr
+  
+}
+
+write.table(Acc, file="/home/varcomp/Landrace_Breed/New_Pipeline/New_Phenotypes/Cross_Validation/F_TeatCount_Acc.csv", quote = F, col.names = F, row.names = F, sep=",")
