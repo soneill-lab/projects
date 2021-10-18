dfb6dcddd63266311c30e7ee85fcb8a277b481e6
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-10-17 23:51:49 -0400

    Create Landrace.LoinArea.Add.CV.sh

---
+++ swine_varcomp/Landrace.LoinArea.Add.CV.sh
@@ -0,0 +1,13 @@
+for data in 1 2 3 4 5 6 7 8 9 10
+do 
+# Get PEBV
+./mmap --ped pedigree.file --phenotype_filename Landrace_LoinArea_Filtered --trait yd --covariate_filename Landrace.IBC.csv --covariates F  --estimate_variance_components --variance_component_filename Landrace.add.grm.bin  Landrace.sow.i.bin  --subject_set Landrace.LoinArea_T$data.csv --variance_component_label A S --file_suffix Landrace.LoinArea_full.CV$data --num_mkl_threads 20 --num_em_reml_burnin 2 --use_em_ai_reml --single_pedigree --use_dpotrs
+
+done
+
+
+# Get inbreeding effect
+./mmap --ped pedigree.file --phenotype_filename Landrace_LoinArea_Filtered --trait yd --covariate_filename Landrace.IBC.csv --covariates F  --estimate_variance_components --variance_component_filename  Landrace.add.grm.bin  Landrace.sow.i.bin --variance_component_label A S --file_suffix Landrace.LoinArea_full.CV.Adj --num_mkl_threads 20 --num_em_reml_burnin 2 --use_em_ai_reml --single_pedigree --use_dpotrs
+
+
+
