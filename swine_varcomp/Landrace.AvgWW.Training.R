9e30f0b63e59485898c327b4c88e21778414397c
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-10-17 21:55:18 -0400

    Create Landrace.AvgWW.Training.R

---
+++ swine_varcomp/Landrace.AvgWW.Training.R
@@ -0,0 +1,15 @@
+library(dplyr)
+
+AvgWW<- ADG_Finisher<- read.table("/home/varcomp/Landrace_Breed/New_Pipeline/New_Phenotypes/Cross_Validation/Average_WeaningWeight_A/Landrace_AverageWeaningWeight_Filtered", header=TRUE, sep=",")
+
+for (i in 1:10){
+  temp<- AvgWW[(1563*(i-1)+1):(1563*i),]  
+  write.table(temp[,1], paste("/home/varcomp/Landrace_Breed/New_Pipeline/New_Phenotypes/Cross_Validation/ADG_Finisher_A/", "Landrace.AvgWW_N", i, ".csv", sep=""), quote = F, row.names = F, col.names = F, sep=",")
+ 
+}
+
+for (i in 1:10){
+  temp<- AvgWW[-((1563*(i-1)+1):(1563*i)),]  
+  write.table(temp[,1], paste("/home/varcomp/Landrace_Breed/New_Pipeline/New_Phenotypes/Cross_Validation/Average_WeaningWeight_A/", "Landrace.AvgWW_T", i, ".csv", sep=""), quote = F, row.names = F, col.names = F, sep=",")
+ 
+}
