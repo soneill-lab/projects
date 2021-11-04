448732fb21ec54e8b11a6fb2e16d40a6e41cbde7
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-11-04 12:52:48 -0400

    Update ADG.TrainingSet.R

--- swine_varcomp/ADG.TrainingSet.R
+++ swine_varcomp/ADG.TrainingSet.R
@@ -4,12 +4,12 @@ ADG<- read.table("/home/varcomp/Landrace_Breed/New_Pipeline/New_Phenotypes/Cross
 
 for (i in 1:10){
   temp<- sample(ADG$IID,(1564*(i-1)+1):(1564*i), replace=FALSE)  
-  write.table(temp[,1], paste("/home/varcomp/Landrace_Breed/New_Pipeline/New_Phenotypes/Cross_Validation/New_CV/ADG_A/", "Landrace.ADG_N", i, ".csv", sep=""), quote = F, row.names = F, col.names = F, sep=",")
+  write.table(temp, paste("/home/varcomp/Landrace_Breed/New_Pipeline/New_Phenotypes/Cross_Validation/New_CV/ADG_A/", "Landrace.ADG_N", i, ".csv", sep=""), quote = F, row.names = F, col.names = F, sep=",")
  
 }
 
 for (i in 1:10){
-  -temp  
-  write.table(temp[,1], paste("/home/varcomp/Landrace_Breed/New_Pipeline/New_Phenotypes/Cross_Validation/New_CV/ADG_A/", "Landrace.ADG_T", i, ".csv", sep=""), quote = F, row.names = F, col.names = F, sep=",")
+  temp2<- -temp  
+  write.table(temp2, paste("/home/varcomp/Landrace_Breed/New_Pipeline/New_Phenotypes/Cross_Validation/New_CV/ADG_A/", "Landrace.ADG_T", i, ".csv", sep=""), quote = F, row.names = F, col.names = F, sep=",")
  
 }
