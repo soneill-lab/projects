ee6cc42d901aa50be7989386da76d39c6a8b8080
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-11-04 13:08:48 -0400

    Update ADG.TrainingSet.R

--- swine_varcomp/ADG.TrainingSet.R
+++ swine_varcomp/ADG.TrainingSet.R
@@ -3,13 +3,13 @@ library(dplyr)
 ADG<- read.table("/home/varcomp/Landrace_Breed/New_Pipeline/New_Phenotypes/Cross_Validation/New_CV/ADG_A/Landrace_ADG_Filtered", header=TRUE, sep=",")
 
 for (i in 1:10){
-  temp<- sample(ADG$IID,(1564*(i-1)+1):(1564*i), replace=FALSE)  
+  temp<- sample(ADG,(1564*(i-1)+1):(1564*i), replace=FALSE)  
   write.table(temp, paste("/home/varcomp/Landrace_Breed/New_Pipeline/New_Phenotypes/Cross_Validation/New_CV/ADG_A/", "Landrace.ADG_N", i, ".csv", sep=""), quote = F, row.names = F, col.names = F, sep=",")
  
 }
 
 for (i in 1:10){
-  temp2<- ADG$IID[-temp]  
+  temp2<- ADG[-temp]  
   write.table(temp2, paste("/home/varcomp/Landrace_Breed/New_Pipeline/New_Phenotypes/Cross_Validation/New_CV/ADG_A/", "Landrace.ADG_T", i, ".csv", sep=""), quote = F, row.names = F, col.names = F, sep=",")
  
 }
