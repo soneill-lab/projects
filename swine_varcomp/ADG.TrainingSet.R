63bb02523e1099230020f0a30ba94c2a900f35ee
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-11-04 12:45:38 -0400

    Update ADG.TrainingSet.R

--- swine_varcomp/ADG.TrainingSet.R
+++ swine_varcomp/ADG.TrainingSet.R
@@ -9,7 +9,7 @@ for (i in 1:10){
 }
 
 for (i in 1:10){
-  temp<- ADG[-((1564*(i-1)+1):(1564*i)),]  
+  -temp  
   write.table(temp[,1], paste("/home/varcomp/Landrace_Breed/New_Pipeline/New_Phenotypes/Cross_Validation/", "Landrace.ADG_T", i, ".csv", sep=""), quote = F, row.names = F, col.names = F, sep=",")
  
 }
