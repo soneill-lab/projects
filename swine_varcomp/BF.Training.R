58f2cf153fa8ceeb25d8ec134a915c23749a8a1b
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-11-04 11:37:31 -0400

    Update BF.Training.R

--- swine_varcomp/BF.Training.R
+++ swine_varcomp/BF.Training.R
@@ -9,7 +9,7 @@ for (i in 1:10){
 }
 
 for (i in 1:10){
-  temp<- BF[-((1564*(i-1)+1):(1564*i)),]  
+  temp<- BF[-((1620*(i-1)+1):(1620*i)),]  
   write.table(temp[,1], paste("/home/varcomp/Landrace_Breed/New_Pipeline/New_Phenotypes/Cross_Validation/", "Landrace.BF_T", i, ".csv", sep=""), quote = F, row.names = F, col.names = F, sep=",")
  
 }
