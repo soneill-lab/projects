7ebab3e55a5d221872cda9aba4f5fd1f3e36e322
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-11-05 14:22:23 -0400

    Create Yorkshire.ADG.Training.R

---
+++ swine_varcomp/Yorkshire.ADG.Training.R
@@ -0,0 +1,14 @@
+library(dplyr)
+
+ADG<- read.table("/home/varcomp/Yorkshire_Breed/Cross_Validation/ADG_A/Yorkshire_ADG_Filtered", header=TRUE, sep=",")
+
+for (i in 1:10){
+  temp<- ADG[(1449*(i-1)+1):(1449*i),]  
+  write.table(temp[,1], paste("/home/varcomp/Yorkshire_Breed/Cross_Validation/ADG_A/", "Yorkshire.ADG_N", i, ".csv", sep=""), quote = F, row.names = F, col.names = F, sep=",")
+ 
+}
+
+for (i in 1:10){
+  temp<- ADG[-((1449*(i-1)+1):(1449*i)),]  
+  write.table(temp[,1], paste("/home/varcomp/Yorkshire_Breed/Cross_Validation/ADG_A/", "Yorkshire.ADG_T", i, ".csv", sep=""), quote = F, row.names = F, col.names = F, sep=",")
+ 
