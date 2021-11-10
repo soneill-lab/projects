edeeb819026ab40a4d6df652c7f385b19ac5f367
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-11-10 10:15:23 -0500

    Create Yorkshire.ADG_Finisher.Training.R

---
+++ swine_varcomp/Yorkshire.ADG_Finisher.Training.R
@@ -0,0 +1,15 @@
+library(dplyr)
+
+ADG_Finisher<- read.table("/home/varcomp/Yorkshire_Breed/Cross_Validation/ADG_Finisher_A/Yorkshire_ADG_Finisher", header=TRUE, sep=",")
+
+for (i in 1:10){
+  temp<- ADG_Finisher[(1460*(i-1)+1):(1460*i),]  
+  write.table(temp[,1], paste("/home/varcomp/Yorkshire_Breed/Cross_Validation/ADG_Finisher_A/", "Yorkshire.ADG_Finisher_N", i, ".csv", sep=""), quote = F, row.names = F, col.names = F, sep=",")
+ 
+}
+
+for (i in 1:10){
+  temp<- ADG_Finisher[-((1460*(i-1)+1):(1460*i)),]  
+  write.table(temp[,1], paste("/home/varcomp/Yorkshire_Breed/Cross_Validation/ADG_Finisher_A/", "Yorkshire.ADG_Finisher_T", i, ".csv", sep=""), quote = F, row.names = F, col.names = F, sep=",")
+ 
+}
