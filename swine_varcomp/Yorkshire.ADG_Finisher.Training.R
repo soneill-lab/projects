c038b0953a5380977231c2c1b9e72df94c63a281
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-11-10 10:17:17 -0500

    Update Yorkshire.ADG_Finisher.Training.R

--- swine_varcomp/Yorkshire.ADG_Finisher.Training.R
+++ swine_varcomp/Yorkshire.ADG_Finisher.Training.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
-ADG_Finisher<- read.table("/home/varcomp/Yorkshire_Breed/Cross_Validation/ADG_Finisher_A/Yorkshire_ADG_Finisher", header=TRUE, sep=",")
+ADG_Finisher<- read.table("/home/varcomp/Yorkshire_Breed/Cross_Validation/ADG_Finisher_A/Yorkshire_ADG_Finisher_Filtered", header=TRUE, sep=",")
 
 for (i in 1:10){
   temp<- ADG_Finisher[(1460*(i-1)+1):(1460*i),]  
