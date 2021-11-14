9c278bfd26e3e9cdc8e853b2620c9abb8cac5ff6
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-11-14 12:55:48 -0500

    Update Yorkshire.ADG_Nursery.Training.R

--- swine_varcomp/Yorkshire.ADG_Nursery.Training.R
+++ swine_varcomp/Yorkshire.ADG_Nursery.Training.R
@@ -3,13 +3,13 @@ library(dplyr)
 ADG_Nursery<- read.table("/home/varcomp/Yorkshire_Breed/Cross_Validation/ADG_Nursery_A/Yorkshire_ADG_Nursery_Filtered", header=TRUE, sep=",")
 
 for (i in 1:10){
-  temp<- ADG_Nursery[(1563*(i-1)+1):(1563*i),]  
+  temp<- ADG_Nursery[(1523*(i-1)+1):(1523*i),]  
   write.table(temp[,1], paste(/"home/varcomp/Yorkshire_Breed/Cross_Validation/ADG_Nursery_A", "Yorkshire.ADG_Nursery_N", i, ".csv", sep=""), quote = F, row.names = F, col.names = F, sep=",")
  
 }
 
 for (i in 1:10){
-  temp<- ADG_Nursery[-((1563*(i-1)+1):(1563*i)),]  
+  temp<- ADG_Nursery[-((1523*(i-1)+1):(1523*i)),]  
   write.table(temp[,1], paste("/home/varcomp/Yorkshire_Breed/Cross_Validation/ADG_Nursery_A", "Yorkshire.ADG_Nursery_T", i, ".csv", sep=""), quote = F, row.names = F, col.names = F, sep=",")
  
 }
