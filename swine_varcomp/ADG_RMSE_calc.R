0b937fd60be22a5b17397543d6785bba2653e783
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2022-02-23 12:13:25 -0500

    Update ADG_RMSE_calc.R

--- swine_varcomp/ADG_RMSE_calc.R
+++ swine_varcomp/ADG_RMSE_calc.R
@@ -7,7 +7,6 @@ ADG_temp<- read.table(paste("/home/varcomp/Yorkshire_Cross_Validation/ADG_F/ADG"
 temp_cor1 <- rmse(ADG_temp[,6], ADG_temp[,5])
 temp_cor2 <- rmse(ADG_temp[,7], ADG_temp[,5])
 result[i,1] <- temp_cor1
-result[i,2] <- temp_cor2  
+result[i,2] <- temp_cor2
 }  
-
-
+write.csv(result, file="ADG_RMSE", row.names=FALSE")
