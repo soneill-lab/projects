395e9de9ddaf478a29eb0087d85654519fb07c8c
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2022-02-23 11:59:58 -0500

    Update RMSE_calc.R

--- swine_varcomp/RMSE_calc.R
+++ swine_varcomp/RMSE_calc.R
@@ -1,6 +1,13 @@
-install.packages("
+install.packages("Metrics")
+library(Metrics)
 
+result <- matrix(NA, 10, 2)
 for (i in 1:10){
 ADG_temp<- read.table(paste("/home/varcomp/Yorkshire_Cross_Validation/ADG_F/ADG",i,"_y.csv", sep=""), header = F, sep=",")
-ADG_corr<-cor(ADG_temp[,5], ADG_temp[,6])
-}
+temp_cor1 <- rmse(ADG_temp[,6], ADG_temp[,5])
+temp_cor2 <- rmse(ADG_temp[,7], ADG_temp[,5])
+result[i,1] <- temp_cor1
+result[i,2] <- temp_cor2  
+}  
+
+
