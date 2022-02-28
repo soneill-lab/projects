dd79a1a6b6cfc61d32d581be2705fed5b8e656d7
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2022-02-28 14:18:35 -0500

    Update RMSE_calc.R

--- swine_varcomp/RMSE_calc.R
+++ swine_varcomp/RMSE_calc.R
@@ -10,13 +10,13 @@ for (i in 1:10){
 ADG_temp<- read.table(paste("/home/varcomp/Yorkshire_Cross_Validation/",trt[t], "_F/", trt[t],i,"_y.csv", sep=""), header = F, sep=",")
 temp_cor1 <- rmse(ADG_temp[,6], ADG_temp[,5])
 temp_cor2 <- rmse(ADG_temp[,7], ADG_temp[,5])
-result[i+10*t,1] <- temp_cor1
-result[i+10*t,2] <- temp_cor2
+result[i+10*(t-1),1] <- temp_cor1
+result[i+10*(t-1),2] <- temp_cor2
 
 # additive model
 ADG_temp<- read.table(paste("/home/varcomp/Yorkshire_Cross_Validation/",trt[t], "_A/", trt[t],i,"_y.csv", sep=""), header = F, sep=",")
 temp_cor1 <- rmse(ADG_temp[,6], ADG_temp[,5])
-result[i+10*t,3] <- temp_cor1
+result[i+10*(t-1),3] <- temp_cor1
 
 }  
 }
